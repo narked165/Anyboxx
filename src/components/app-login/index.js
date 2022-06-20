d6a50c4ba1bf568c4fdcc1a8950ad734a69da01e
Paul H Fabing <narked165@gmail.com>
2022-06-19 18:42:30 -0700

    fixed css issues

--- src/components/app-login/index.js
+++ src/components/app-login/index.js
@@ -1,9 +1,18 @@
 import { default as Controller } from '../../scripts/controller.js'
+const icons = {
+    exclaim: "<span class='icon-exclaim'>&#10071;</span>",
+    error: "<span class='icon-error'>&#10060;</span>",
+    ok: "<span class='icon-ok'>&#9989;</span>"
+}
 
+const supportDialog = {
+    required: `${ icons.exclaim } <span class="required">Required!</span>`
+}
 const appLogin = Controller('app-login', appLogin => {
     appLogin.setAttribute('formmethod', 'post')
     appLogin.setAttribute('action', '/a/verifyUser')
 })
+
 const tLoginTitle = Controller('t-login-title', tLoginTitle => {
     tLoginTitle.innerHTML="Login To Anyboxx"
 })
@@ -21,7 +30,10 @@ const iLoginUsername = Controller('i-login-username', iLoginUsername => {
 })
 
 const oLoginUsername = Controller('o-login-username', oLoginUsername => {
+    oLoginUsername.on('empty-field', () => {
+        oLoginUsername.innerHTML=supportDialog.required
 
+    })
 })
 
 const cLoginPassword = Controller('c-login-password', cLoginPassword => {
@@ -38,7 +50,9 @@ const iLoginPassword = Controller('i-login-password', iLoginPassword => {
 })
 
 const oLoginPassword = Controller('o-login-password', oLoginPassword => {
-
+    oLoginPassword.on('empty-field', () => {
+        oLoginPassword.innerHTML=supportDialog.required
+    })
 })
 
 const cLoginControl = Controller('c-login-control', cLoginControl => {
@@ -60,12 +74,15 @@ const cLoginSupport = Controller('c-login-support', cLoginSupport => {
 
 })
 
-const aLoginNewAccount = Controller('aLoginNewAccount', aLoginNewAccount => {
+const aLoginNewAccount = Controller('a-login-new-account', aLoginNewAccount => {
     aLoginNewAccount.setAttribute('href', '#createAccount')
     aLoginNewAccount.innerHTML= "Create a new account"
 })
 
 const aLoginForgot = Controller('a-login-forgot', aLoginForgot => {
-    aLoginNewAccount.setAttribute('href', '#forgotLoginInfo')
-    aLoginNewAccount.innerHTML= "Forgot login credentials"
+    aLoginForgot.setAttribute('href', '#forgotLoginInfo')
+    aLoginForgot.innerHTML= "Forgot login credentials"
 })
+
+oLoginPassword.emit('empty-field')
+oLoginUsername.emit('empty-field')
